1bf234084009b94f9766078e3f27db4e08426ae8
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2023-12-19 19:09:05 +0530

    Explored File System based routing
    Created login folder for a login page (page.js)
    Created about folder for an about us page (page.js)

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h1>About Us</h1>
+            </div>
+        </main>
+    )
+}
+
+export default About;
---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+const Login = () => {
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h1>Login Page</h1>
+            </div>
+        </main>
+    )
+}
+
+export default Login;
