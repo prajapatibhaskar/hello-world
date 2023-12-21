d5e97bd497ac04696224af35ed6bd67f0626459d
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2023-12-21 23:14:32 +0530

    One more Catch-All-Segments (With Optional Catch-All-Segments)
    Also made use of Optional Chaining ?.
    To handle 'cannot read properties of undefined' error.

---
+++ src/app/docs/[[...slug]]/page.js
@@ -0,0 +1,20 @@
+import { Londrina_Outline } from "next/font/google";
+
+const Docs = ({ params }) => {
+  if (params.slug?.length === 2) {
+    return (
+      <h1>
+        Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
+      </h1>
+    );
+  } else if (params.slug?.length === 1){
+    return (
+      <h1>
+        Viewing docs for feature {params.slug[0]}
+      </h1>
+    );
+  }
+  return <h1>Viewing Docs Home Page</h1>
+};
+
+export default Docs;
