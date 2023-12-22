48b65eef5de91e333bec171fd79b51c3909170f2
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2023-12-22 22:36:36 +0530

    Added a Private Folder named _lib

---
+++ src/app/_lib/page.js
@@ -0,0 +1,10 @@
+//normally this page.js file would correspond to url 'localhost:3000/_lib'
+//but it doesnt because of underscore before file name :)
+
+const PrivateRoute = () => {
+    return(
+        <h1>You cannot view this in the browser</h1>
+    )
+}
+
+export default PrivateRoute;
