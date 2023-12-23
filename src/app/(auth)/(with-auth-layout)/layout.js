b45f8b1ca9c1a12f8e9b71b56e6ad26b9711d73d
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2023-12-23 14:34:43 +0530

    Another usage of Route Group
    For selectively applying a layout,
    to certain segments while leaving others unchanged

---
+++ src/app/(auth)/(with-auth-layout)/layout.js
@@ -0,0 +1,11 @@
+const AuthLayout = ({ children }) => {
+  return (
+    <div>
+      <h2>------------------Authorization Layout------------------</h2>
+      {children}
+      <h2>------------------Authorization Layout------------------</h2>
+    </div>
+  );
+}
+
+export default AuthLayout;
