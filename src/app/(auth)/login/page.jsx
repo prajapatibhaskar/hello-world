e394dd1f3c8a135a3af92fc0139556429ae301bf
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2023-12-23 23:55:38 +0530

    Loading UI

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -13,9 +13,9 @@ const Login = () => {
       <div  style={{padding: '20px'}}>
         <h1 className="heading">Login Page</h1>
         <br />
-        <button onClick={() => navigate("stafflogin")}>Staff Login</button>
+        <button onClick={() => navigate("stafflogin")} class="button-39" role="button">Staff Login</button>
         <br /> <br />
-        <button onClick={() => navigate("studentlogin")}>Student Login</button>
+        <button onClick={() => navigate("studentlogin")} class="button-39" role="button">Student Login</button>
       </div>
   );
 };
--- src/app/order-product/page.jsx
+++ src/app/order-product/page.jsx
@@ -1,23 +1,24 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 
 const OrderProduct = () => {
+  // for routing 'programatically'
+  const router = useRouter();
 
-    // for routing 'programatically'
-    const router = useRouter()
-
-    const handleClick = () => {
-        console.log('Placing your order');
-        router.push('/home')
-    }
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/home");
+  };
 
   return (
     <>
-        <h1>Order Product</h1>
-        <button onClick={handleClick}>Place Order</button>
+      <h1>Order Product</h1>
+      <button onClick={handleClick} class="button-39" role="button">
+        Place Order
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default OrderProduct
+export default OrderProduct;
