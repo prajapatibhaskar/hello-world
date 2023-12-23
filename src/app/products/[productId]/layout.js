034628112153d7a638e9a03a1f6e72cbb949eaea
Bhaskar Prajapati <prajapatibhaskarbp@gmail.com>
2023-12-23 14:21:45 +0530

    Explored Nested Layout
    First Global layout is rendered,
    Then the nested layout is rendered within it

---
+++ src/app/products/[productId]/layout.js
@@ -0,0 +1,10 @@
+const ProductDetailsLayout = ({children}) => {
+    return(
+        <div>
+            {children}
+            <h1>Featured Products...</h1>
+        </div>
+    )
+}
+
+export default ProductDetailsLayout;
